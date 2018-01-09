ce475e2c550cd8c8f2f8b449e048ee7d87018124
DESKTOP-6PLQ176\JH <wkdwlgus901@naver.com>
2018-01-09 14:01:50 +0900

    A-B

---
+++ num1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class num1001 {
+
+	public static void main(String[] args) {
+		
+		int T;
+		int N;
+		
+		Scanner scanner = new Scanner(System.in);
+		T = scanner.nextInt();
+		
+		
+		 
+		
+	}
+}
