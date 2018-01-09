eef94633f47c63e289e695046ada194ab980c0a8
DESKTOP-6PLQ176\JH <wkdwlgus901@naver.com>
2018-01-09 14:01:31 +0900

    A+B

---
+++ num1000.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class num1000 {
+
+	public static void main(String[] args) {
+		
+		int num1,num2;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("");
+		num1 = scan.nextInt();
+		num2 = scan.nextInt();
+		
+		int sum = num1 + num2;
+		System.out.println(sum);
+		
+	}
+}
