fa4746ee24ecfc54536424444023dc40163f323e
wkdwlgus901 <wlgusghdks1@gmail.com>
2018-11-28 14:36:24 +0900

    틀린 이유... 알수없음 ㅠㅠ

---
+++ implementation/num5032.java
@@ -0,0 +1,16 @@
+package implementation;
+
+import java.util.Scanner;
+
+public class num5032 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int e = scan.nextInt();
+		int f = scan.nextInt();
+		int c = scan.nextInt();
+		
+		System.out.println((e+f)/c);
+	}
+
+}
