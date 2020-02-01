f46d63cf3f15c914505ebae9e7a19fd2434c1228
JH901 <wlgusghdks1@gmail.com>
2020-02-01 16:44:08 +0900

    num 11718/ 11719

---
+++ src/problem/num11718_11719.java
@@ -0,0 +1,16 @@
+package problem;
+
+import java.util.Scanner;
+
+public class num11718_11719 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(scan.hasNextLine()) {
+			String input = scan.nextLine();
+			System.out.println(input);
+		}
+		scan.close();
+	}
+
+}
