62e47e685f2949e269fa2a70f87916939402e29e
JH901 <wlgusghdks1@gmail.com>
2020-02-02 11:35:50 +0900

    NUM11654

---
+++ src/problem/num11654.java
@@ -0,0 +1,17 @@
+package problem;
+
+import java.util.Scanner;
+
+public class num11654 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		
+		char i = input.charAt(0);
+		int code = (int)i;
+		
+		System.out.println(code);
+	}
+
+}
